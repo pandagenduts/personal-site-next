3187965b1f9617b76ec6eecb17cd84091ee191ee
Donny Rendi <donny.rendi@yahoo.co.id>
2023-11-20 10:09:17 +0700

    add useEffect to body query selector

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,25 @@ import Hero from '@/components/home/Hero';
 import Portfolio from '@/components/home/Portfolio';
 import Modal from '@/components/modal/Modal';
 import { useAppSelector } from '@/redux/hooks';
+import { useEffect } from 'react';
 
-const body = document.querySelector<HTMLBodyElement>('body');
+
 
 export default function Home() {
   const { isShowModal } = useAppSelector((state) => state.theModal);
 
-  // remove the scrollbar when modal is shown
-  if (body) {
-    if (isShowModal) {
-      body.classList.add('active');
-    } else {
-      body.classList.remove('active');
+  useEffect(() => {
+    const body = document.querySelector<HTMLBodyElement>('body');
+    if (body) {
+      if (isShowModal) {
+        body.classList.add('active');
+      } else {
+        body.classList.remove('active');
+      }
     }
-  }
+  })
+  // remove the scrollbar when modal is shown
+
 
   return (
     <>
