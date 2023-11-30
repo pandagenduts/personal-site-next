4e0e8c25688dceecd3fcaa0012aadc81796305b5
Donny Rendi <donny.rendi@yahoo.co.id>
2023-11-30 09:53:46 +0700

    default portfolio button active done

--- components/home/PortfolioButton.tsx
+++ components/home/PortfolioButton.tsx
@@ -1,24 +1,28 @@
-'use client'
+'use client';
 
 type Props = {
   activeCardCategory: string;
   category: string;
   updateSearchParams: (category: string) => void;
   children: React.ReactNode;
-}
+};
 export default function PortfolioButton(props: Props) {
-  const {activeCardCategory, category, updateSearchParams, children } = props;
+  const { activeCardCategory, category, updateSearchParams, children } = props;
 
   return (
     <button
-    onClick={() => {
-      updateSearchParams(category);
-    }}
-    className={`duration-150 hover:text-bluePrimary ${
-      activeCardCategory === category ? 'text-bluePrimary' : ''
-    }`}
-  >
-    {children}
-  </button>
-  )
+      onClick={() => {
+        updateSearchParams(category);
+      }}
+      className={`duration-150 hover:text-bluePrimary ${
+        activeCardCategory === category ? 'text-bluePrimary' : ''
+      } ${
+        activeCardCategory === '' && category === 'react-nextjs'
+          ? 'text-bluePrimary'
+          : ''
+      }`}
+    >
+      {children}
+    </button>
+  );
 }
